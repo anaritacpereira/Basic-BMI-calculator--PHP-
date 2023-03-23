ca120f4ea76e166f7148835863f36ae6157fcb7d
anaritacpereira <anaritacpereira.work@gmail.com>
2023-03-23 16:27:34 +0000

    Refresh

--- entrada.php
+++ entrada.php
@@ -10,7 +10,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Trabalho 17</title>
+    <title>BMI calculator</title>
     <link rel="stylesheet" href="estilo.css">
 </head>
 <body>
